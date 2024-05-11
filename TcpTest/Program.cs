ed033cae4f1ad5c4b65de9fe0dab41b58c15a28d
mihkl <mihkelj18@gmail.com>
2024-05-11 05:27:59 +0300

    Add project files.

---
+++ TcpTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TcpTest {
+    internal class Program {
+        static void Main(string[] args) {
+            TcpClient client = new("127.0.0.1", 4948);
+            NetworkStream stream = client.GetStream();
+
+            string jsonMessage = "{\"Qualifier\":\"showWinningNumber\",\"Data\":{\"WinningNumber\":24}}";
+            byte[] buffer = Encoding.ASCII.GetBytes(jsonMessage);
+            stream.Write(buffer, 0, buffer.Length);
+
+            client.Close();
+        }
+    }
+}
